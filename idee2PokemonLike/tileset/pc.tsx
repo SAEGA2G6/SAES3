6564fbfb2261567c5c6ba48e50a00b5f16898d2b
Ethan TESTA <104560706+Aehnt@users.noreply.github.com>
2023-01-07 11:43:35 +0100

    Delete idee2PokemonLike directory

--- idee2PokemonLike/tileset/pc.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="pc" tilewidth="32" tileheight="32" tilecount="2" columns="2">
- <properties>
-  <property name="estSolide" type="bool" value="true"/>
- </properties>
- <image source="pc.png" width="95" height="40"/>
-</tileset>
--- idee2PokemonLike/tileset/wall-tiles-autotile.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="wall-tiles-autotile" tilewidth="32" tileheight="32" tilecount="55" columns="11">
- <properties>
-  <property name="estSolide" type="bool" value="true"/>
- </properties>
- <image source="wall-tiles-autotile.png" width="352" height="160"/>
-</tileset>
