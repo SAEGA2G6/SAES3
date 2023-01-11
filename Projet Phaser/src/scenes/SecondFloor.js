7e8fe8eab2f6416d14a538a4f11412000432a70d
m20007529 <aurelien.mattera@etu.univ-amu.fr>
2023-01-11 17:24:29 +0100

    implémentation 2ème étage

--- Projet Phaser/src/scenes/SecondFloor.js
+++ Projet Phaser/src/scenes/SecondFloor.js
@@ -1,7 +1,11 @@
 class SecondFloor extends Phaser.Scene {
-    constructor() {
-      super("SecondFloor");
-    }
+  init(data) {
+    this.playerGender = data.texture;
+    this.playerPseudo = data.pseudo;
+    this.currentNbRoom = 1;
+    this.nbRooms = 6; //TODO: mettre nb de rooms avec boss de l'étage
+    this.levelPrefix = "e2" //TODO: mettre le prefixe de la room
+  }
   
     /** @returns {void} */
     editorCreate() {
@@ -19,6 +23,7 @@ class SecondFloor extends Phaser.Scene {
       const tileset6 = carte.addTilesetImage("portes", "portes");
       const tileset7 = carte.addTilesetImage("poubelle", "poubelle");
       const tileset8 = carte.addTilesetImage("toiletsBureau", "toiletsBureau");
+      const tileset9 = carte.addTilesetImage("sofa", "sofa");
   
       const tilesets_list = [
         tileset1,
@@ -29,6 +34,7 @@ class SecondFloor extends Phaser.Scene {
         tileset6,
         tileset7,
         tileset8,
+        tileset9
       ];
   
       ///////////// LAYERS /////////////
@@ -48,20 +54,225 @@ class SecondFloor extends Phaser.Scene {
   
       ///////////// PLAYER /////////////
   
-      const player = new Player(this, 400, 218).setDepth(1);
+      const player = new Player(this, 160, 600, this.playerGender).setDepth(1);
       this.player = player;
   
       ///////////// PROF/BOSS /////////////
       
-      //TODO//
-  
+      const prof10 = new DialogObject(
+        this,
+        788,
+        400,
+        "prof10",
+        "Appuyer sur ESPACE pour commencer le QCM !",
+        null,
+        "mcq"
+      );
+
+      const prof11 = new DialogObject(
+        this,
+        1870,
+        200,
+        "prof1",
+        "Appuyer sur ESPACE pour commencer le QCM !",
+        null,
+        "mcq"
+      );
+
+      const prof12 = new DialogObject(
+        this,
+        1010,
+        609,
+        "prof9",
+        "Appuyer sur ESPACE pour commencer le QCM !",
+        null,
+        "mcq"
+      );
+
+      const prof13 = new DialogObject(
+        this,
+        1232,
+        580,
+        "prof8",
+        "Appuyer sur ESPACE pour commencer le QCM !",
+        null,
+        "mcq"
+      );
+
+      const prof14 = new DialogObject(
+        this,
+        1552,
+        580,
+        "prof11",
+        "Appuyer sur ESPACE pour commencer le QCM !",
+        null,
+        "mcq"
+      );
+
+        const prof15 = new DialogObject(
+        this,
+        1872,
+        580,
+        "prof6",
+        "Appuyer sur ESPACE pour commencer le QCM !",
+        null,
+        "mcq"
+      );
+
+
       ///////////// DOORS /////////////
       
-      //TODO//
+      const door_room2 = new Door(this, 1888, 417, "doubleporte", true);
+
+      const door_room3 = new Door(this, 800, 545, "doubleporte", false);
+
+      const door_room4 = new Door(this, 1120, 545, "doubleporte", false);
+
+      const door_room5 = new Door(this, 1440, 545, "doubleporte", false);
+
+      const door_room6 = new Door(this, 1760, 545, "doubleporte", false);
   
       ///////////// INDICES /////////////
+
+      /////////////// ROOM 1 /////////////
+
       
-      //TODO//
+      const bin_room1 = new DialogObject(
+        this, 620, 400,
+        "poubelleSprite",
+        "Appuyer sur ESPACE pour fouiller la poubelle","e2_r1_1",
+        "clue"
+      );
+
+      const papers_room1 = new DialogObject(
+        this, 755, 225,
+        "papiers",
+        "Appuyer sur ESPACE pour regarder les notes...","e2_r1_2",
+        "clue"
+      );
+
+      const eleve_room1 = new DialogObject(
+        this, 656, 108,
+        "eleve3",
+        "Appuyer sur ESPACE pour parler à l'élève","e2_r1_3",
+        "clue"
+      );
+
+      /////////////// ROOM 2 /////////////
+
+      const pcOn1_room2 = new DialogObject(
+        this, 1743, 238,
+        "pcAllume",
+        "Appuyer sur ESPACE pour regarder l'ordinateur...","e2_r2_1",
+        "clue"
+      );
+
+
+      const papers_room2 = new DialogObject(
+        this, 1840, 310,
+        "papiers",
+        "Appuyer sur ESPACE pour regarder les notes...","e2_r2_2",
+        "clue"
+      );
+
+      const eleve_room2 = new DialogObject(
+        this, 1872, 45,
+        "eleve4",
+        "Appuyer sur ESPACE pour parler à l'élève","e2_r2_3",
+        "clue"
+      );
+
+      /////////////// ROOM 3 /////////////
+
+      const bin_room3 = new DialogObject(
+        this, 750, 590,
+        "poubelleSprite",
+        "Appuyer sur ESPACE pour fouiller la poubelle","e2_r3_3",
+        "clue"
+      );
+
+      const papers_room3 = new DialogObject(
+        this, 782, 792,
+        "papiers",
+        "Appuyer sur ESPACE pour regarder les notes...","e2_r3_2",
+        "clue"
+      );
+
+      const papers2_room3 = new DialogObject(
+        this, 910, 920,
+        "papiers",
+        "Appuyer sur ESPACE pour regarder les notes...","e2_r3_2",
+        "clue"
+      );
+
+      /////////////// ROOM 4 //////320//////
+
+      const eleve_room4 = new DialogObject(
+        this, 1070, 710,
+        "eleve1",
+        "Appuyer sur ESPACE pour fouiller la poubelle","e2_r2_1",
+        "clue"
+      );
+
+      const papers_room4 = new DialogObject(
+        this, 1326, 656,
+        "papiers",
+        "Appuyer sur ESPACE pour regarder les notes...","e2_r4_2",
+        "clue"
+      );
+
+      const papers2_room4 = new DialogObject(
+        this, 1298, 920,
+        "papiers",
+        "Appuyer sur ESPACE pour regarder les notes...","e2_r4_3",
+        "clue"
+      );
+
+      /////////////// ROOM 5 /////////////
+
+      const bin_room5 = new DialogObject(
+        this, 1390, 590,
+        "poubelleSprite",
+        "Appuyer sur ESPACE pour fouiller la poubelle","e2_r5_1",
+        "clue"
+      );
+
+      const papers_room5 = new DialogObject(
+        this, 1522, 796,
+        "papiers",
+        "Appuyer sur ESPACE pour regarder les notes...","e2_r4_3",
+        "clue"
+      );
+
+      const eleve_room5 = new DialogObject(
+        this, 1390, 710,
+        "eleve2",
+        "Appuyer sur ESPACE pour fouiller la poubelle","e2_r2_1",
+        "clue"
+      );
+
+      /////////////// ROOM 6 /////////////
+
+      const papers_room6 = new DialogObject(
+        this, 1965, 930,
+        "papiers",
+        "Appuyer sur ESPACE pour regarder les notes...","e2_r4_3",
+        "clue"
+      );
+
+      const bin_room6 = new DialogObject(
+        this, 1710, 590,
+        "poubelleSprite",
+        "Appuyer sur ESPACE pour fouiller la poubelle","e2_r5_1",
+        "clue"
+      );
+
+      const eleve_room6 = new DialogObject(
+        this, 1904, 902,
+        "eleve3",
+        "Appuyer sur ESPACE pour fouiller la poubelle","e2_r2_1",
+        "clue"
+      );
   
       ///////////// COLLISIONS /////////////
   
@@ -72,7 +283,15 @@ class SecondFloor extends Phaser.Scene {
       const collider_list = [
         calque1,
         calque2,
-        calque3
+        calque3,
+        door_room2, door_room3, door_room4, door_room5, door_room6,
+        prof10, prof11, prof12, prof13, prof14, prof15,
+        bin_room1, eleve_room1,
+        eleve_room2,
+        bin_room3, 
+        eleve_room4,
+        bin_room5, eleve_room5,
+        bin_room6, eleve_room6
       ];
       this.physics.add.collider(player, collider_list);
   
