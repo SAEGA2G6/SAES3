81a53f5ae8c708e3862f3f6bae7bf0630785e81b
Thibault-De-Permentier <thibault.DE-PERMENTIER@etu.univ-amu.fr>
2023-02-02 15:05:53 +0100

    ajout première version d'une page administration

---
+++ Projet Phaser/Administration.js
@@ -0,0 +1,17 @@
+function afficheEditeur(){
+document.write('<div><form><p> id question : sera généré automatiquement</p>'
++'<p>parcour : <textarea></textarea></p>'
++'<p>salle : <textarea>au format [étage]_r[numSalle]</textarea></p>'
++'<p>enoncer : <textarea></textarea></p>'
++'<p>bonne reponse :' 
++'<select>'
++'<option value="1">reponse 1</option>'
++'<option value="2">reponse 2</option>'
++'<option value="3">reponse 3</option>'
++'<option value="4">reponse 4</option>'
++'</select></p>'
++'<p>reponse 1 : <textarea></textarea></p>'
++'<p>reponse 2 : <textarea></textarea></p>'
++'<p>reponse 3 : <textarea></textarea></p>'
++'<p>reponse 4 : <textarea></textarea></p>'
++'<p><input type="submit" value="Envoyer" ></p></form></div>');}
---
+++ Projet Phaser/Administration.php
@@ -0,0 +1,75 @@
+<?php
+echo'<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta description="Serious Game présentant le BUT Informatique d\'Aix-En-Provence !">
+    <title>Aix\'ploration - À la découverte du BUT Informatique !</title>
+</head>
+<body>
+    <header>
+
+    </header>';
+if ($_POST['login']=='admin'){
+    if ($_POST['password']=='passwordAdministrator'){
+        // on se connecte à notre base
+        $db = new mysqli("mysql-aixploration.alwaysdata.net", "292885", "aixploration2023", "aixploration_sql");
+ 
+        // lancement de la requête
+        $sql = 'SELECT * FROM QUESTION';
+     
+        // on lance la requête (mysql_query) et on impose un message d'erreur si la requête ne se passe pas bien (or die)
+        $res = mysqli_query($db, $sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysqli_error($db));
+     
+     
+        //$res = mysqli_query($db, $sql);
+        if(mysqli_num_rows($res) !==0) {
+            while ($data = mysqli_fetch_object($res)) {
+                echo '<div><p> id question : '. $data->ID_QUESTION.'</p>';
+                echo '<p>parcour : '. $data->Parcour.'</p>';
+                echo '<p>salle : '. $data->salle.'</p>';
+                echo '<p>enoncer : "'. $data->Enoncer.'"</p>';
+                echo '<p>bonne reponse : '. $data->BonneReponse.'</p>';
+                echo '<p>reponse 1 : "'. $data->Reponse1.'"</p>';
+                echo '<p>reponse 2 : "'. $data->Reponse2.'"</p>';
+                echo '<p>reponse 3 : "'. $data->Reponse3.'"</p>';
+                echo '<p>reponse 4 : "'. $data->Reponse4.'"</p></div>';
+                echo '<br>';
+                echo '<br>';     
+        }
+        }else {
+        echo 'pas de correspondance';
+        echo '<br>';
+        echo '<br>';     
+        }  
+                 
+        // on libère l'espace mémoire alloué pour cette interrogation de la base
+        mysqli_free_result ($res);
+        mysqli_close ($db);
+
+        echo '<button onclick="afficheEditeur()">crée une nouvelle question</button>';
+        echo '<script src="Adminstration.js"></script>
+        <footer>
+        <a href="/mentionslegales.html">Mentions légales</a><p> . </p><a href="/index.html"> le jeu</a>
+        </footer>
+        </body></html>';
+    }
+    else{
+        echo '<p>mauvais mot de passe</p><br>';
+        echo '<a href="/login.php">page de connexion</a>
+        <footer>
+        <a href="/mentionslegales.html">Mentions légales</a><p> . </p><a href="/index.html"> le jeu</a>
+        </footer>
+        </body></html>';
+    }
+}
+else{
+    echo '<p>mauvais login</p><br>';
+    echo '<a href="/login.php">page de connexion</a>
+    <footer>
+    <a href="/mentionslegales.html">Mentions légales</a><p> . </p><a href="/index.html"> le jeu</a>
+    </footer>
+    </body></html>';
+}
